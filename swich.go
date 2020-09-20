e8720f540336e27c52c4922f89ef6a6ed4f70aff
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-09-20 11:53:14 +0900

    pra swich

---
+++ swich.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func getOsName() string {
+	return "mac"
+}
+func main() {
+
+	switch os := getOsName(); os {
+	case "mac":
+		fmt.Println("Mac!!")
+	case "windows":
+		fmt.Println("Windows!!")
+	default:
+		fmt.Println("Default!!")
+	}
+	t := time.Now()
+	fmt.Println(t.Hour())
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Morning")
+	case t.Hour() < 17:
+		fmt.Println("Afternoon")
+
+	}
+}

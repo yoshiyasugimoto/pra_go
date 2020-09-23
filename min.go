d5313879427c7a550408b05fa8512653dcd4906f
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-09-23 09:43:51 +0900

    fix min.go

--- min.go
+++ min.go
@@ -4,10 +4,22 @@ import "fmt"
 
 func main() {
 	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
-	min := l[0]
-	for _, v := range l {
-		if v < min {
-			min = v
+	//min := l[0]
+	//for _, v := range l {
+	//	if v < min {
+	//		min = v
+	//	}
+	//}
+	//fmt.Println(min)
+	var min int
+
+	for i, num := range l {
+		if i == 0 {
+			min = num
+			continue
+		}
+		if min >= num {
+			min = num
 		}
 	}
 	fmt.Println(min)

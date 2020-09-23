f4a6d8b826d6d15ce21656b2c057cbe487b39b1c
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-09-23 09:27:51 +0900

    pra error

---
+++ error.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./lesson.go")
+	if err != nil {
+		log.Fatalln("Error!")
+	}
+	defer file.Close()
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil{
+		log.Fatalln("Error")
+	}
+	fmt.Println(count, string(data))
+}

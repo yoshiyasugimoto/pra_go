47376ece5b71f18bbdee1960c0fdf31b19811b05
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-09-23 15:33:34 +0900

    fibonach versionUP

--- fibonach.go
+++ fibonach.go
@@ -9,7 +9,16 @@ func Fibo(n int) int {
 	return Fibo(n-2) + Fibo(n-1)
 }
 
+func Fibo2(n int) int {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		x, y = y, x+y
+	}
+	return x
+}
+
 func main() {
 	n := 10
 	fmt.Println(Fibo(n))
+	fmt.Println(Fibo2(n))
 }
